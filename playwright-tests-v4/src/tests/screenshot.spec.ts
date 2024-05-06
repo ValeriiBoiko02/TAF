44ed895314898fd900902841176580887d81c86e
ValeriiBoiko02 <valerii.boiko01@gmail.com>
2024-05-06 14:46:50 +0300

    screenshot tests

---
+++ playwright-tests-v4/src/tests/screenshot.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '../utils/test.fixtures';
+
+test.describe('Home page visual test', { tag: '@screenshot' }, () => {
+  test('Full home page', async ({ homePage, page }) => {
+    homePage; // TODO: delete -> error: homePage is never used
+    await expect(page).toHaveScreenshot('fullPage.png');
+  });
+
+  test('@header Header component', async ({ homePage }) => {
+    await expect(homePage.headerComponent.container).toHaveScreenshot('header.png');
+  });
+});
